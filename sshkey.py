9ecd4591e0c87e20a08d528a1932a72b40432166
nphard001 <nphard001@gmail.com>
2019-04-12 15:32:35 +0800

    ENH: config append

--- sshkey.py
+++ sshkey.py
@@ -1,6 +1,41 @@
+import os
 import sys
+import hashlib
+import tempfile
 import datetime
 import subprocess
+md5 = lambda x: hashlib.md5(x.encode('utf-8')).hexdigest()
 now = lambda: datetime.datetime.now().timestamp()
-run = lambda cmd: subprocess.run(cmd, shell=True)
-run('ssh-keygen -f indev_sshkey -t ecdsa -b 521')
+def run(cmd):
+    print('[run]', cmd, file=sys.stderr)
+    return subprocess.run(cmd, shell=True).returncode
+user_at_host = sys.argv[1]
+symbol = 'key_'+md5(user_at_host)[:7]
+key_dir = '~/.ssh'
+key_private = os.path.join(key_dir, symbol)
+key_public = os.path.join(key_dir, symbol+'.pub')
+
+# remote authorized_keys
+run(f'ssh-keygen -f {key_private} -t ecdsa -b 521')
+run(f'scp {key_public} {user_at_host}:~/.ssh')
+run(f'ssh {user_at_host} "cat {key_public} >> ~/.ssh/authorized_keys"')
+
+# local config file
+lst2 = user_at_host.split('@')
+host = lst2[-1]
+if len(lst2)==2:
+    user = lst2[0]
+else:
+    user = os.getenv('USER')
+
+config = '~/.ssh/config'
+config_to_add = tempfile.NamedTemporaryFile(mode="w")
+config_to_add.file.write('\n'+f'''
+Host {host}
+  HostName {host}
+  User {user}
+  IdentityFile {key_private}
+  IdentitiesOnly yes
+    '''.strip()+'\n')
+config_to_add.file.flush()
+run(f'cat {config_to_add.name} >> {config}')
