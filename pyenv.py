6e75263f9b45d4358141351c3ddb895e5717b6c2
nphard001 <nphard001@gmail.com>
2019-04-13 11:41:08 +0800

    ADD: download miniconda

--- pyenv.py
+++ pyenv.py
@@ -10,3 +10,16 @@ run('sudo ln -s ~/.pyenv/bin/pyenv /usr/local/sbin/pyenv')
 run('git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv')
 run('eval "$(pyenv init -)"')
 run('eval "$(pyenv virtualenv-init -)"')
+run('pyenv install miniconda3-4.3.30')
+run('pyenv virtualenv miniconda3-4.3.30 mini368')
+run('pyenv global mini368')
+run('eval "$(pyenv init -)"')
+run('eval "$(pyenv virtualenv-init -)"')
+
+
+r"""
+➜  .pyenv git:(master) pyenv install miniconda3-4.3.30
+➜  .pyenv git:(master) pyenv virtualenv miniconda3-4.3.30 mini368
+➜  .pyenv git:(master) pyenv global mini368
+
+"""
