39eb5632ecec382430dbf68746343b16ec40de83
nphard001 <nphard001@gmail.com>
2019-05-25 20:49:30 +0800

    ENH: nphard001's ohmyzsh

--- ohmyzsh.py
+++ ohmyzsh.py
@@ -5,4 +5,4 @@ def run(cmd):
     return subprocess.run(cmd, shell=True).returncode
 
 run('sudo apt-get install -y zsh')
-run('sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"')
+run('sh -c "$(wget https://raw.githubusercontent.com/nphard001/oh-my-zsh/master/tools/install.sh -O -)"')
