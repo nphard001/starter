a454f62bca99640ad70a4b24c0ccb24d12da243a
nphard001 <nphard001@gmail.com>
2022-12-23 17:31:03 +0800

    ADD: cuda.sh

---
+++ ub1804/cuda.sh
@@ -0,0 +1,4 @@
+# *source*
+# https://cloud.google.com/compute/docs/gpus/install-drivers-gpu
+curl https://raw.githubusercontent.com/GoogleCloudPlatform/compute-gpu-installation/main/linux/install_gpu_driver.py --output install_gpu_driver.py
+sudo python3 install_gpu_driver.py
