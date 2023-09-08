352efef063ba21dfcc7cb1f8d86862ce1dffe4d3
nphard001 <nphard001@gmail.com>
2023-09-09 07:51:59 +0800

    MISC: a1111 wget Makefile

--- sd/a1111.sh
+++ sd/a1111.sh
@@ -1,3 +1,5 @@
 mkdir -p /active/zip
 mkdir -p /active/tmp
 ln -s /content/stable-diffusion-webui /active/sd
+wget https://raw.githubusercontent.com/nphard001/starter/master/sd/Makefile -O /active/Makefile
+
