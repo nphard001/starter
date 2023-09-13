5187c5b6aca07409e68eff71b707472135804516
nphard001 <nphard001@gmail.com>
2023-09-13 14:34:35 +0800

    ENH: data.makefile

--- sd/a1111.sh
+++ sd/a1111.sh
@@ -1,7 +1,4 @@
-mkdir -p /content/stable-diffusion-webui/models/Stable-diffusion || true
-mkdir -p /content/stable-diffusion-webui/extensions || true
 mkdir -p /active/zip
 mkdir -p /active/tmp
-ln -s /content/stable-diffusion-webui /active/sd
 wget https://raw.githubusercontent.com/nphard001/starter/master/sd/Makefile -O /active/Makefile
 
