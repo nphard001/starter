c9376eb72e4f97a41c64880bdac879c4eaf85b9d
nphard001 <nphard001@gmail.com>
2023-09-20 08:19:10 +0800

    ADD: mount MyDrive for colab

--- sd/a1111.sh
+++ sd/a1111.sh
@@ -1,5 +1,4 @@
 mkdir -p /active
 wget https://raw.githubusercontent.com/nphard001/starter/master/sd/Makefile -O /active/Makefile
-wget https://raw.githubusercontent.com/nphard001/starter/master/sd/upload.py -O /active/upload.py
 wget https://raw.githubusercontent.com/nphard001/starter/master/sd/controlNet.sh -O /active/controlNet.sh
 
